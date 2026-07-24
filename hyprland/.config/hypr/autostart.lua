ae0c8c55107c99aed948b87bebcfe8bfa7c25ba5
belkacem teta <78418285+belkacemteta@users.noreply.github.com>
2026-07-24 23:31:58 +0100

    Do minor changes..

--- hyprland/.config/hypr/autostart.lua
+++ hyprland/.config/hypr/autostart.lua
@@ -1,6 +1,7 @@
 
 hl.on("hyprland.start", function ()
-  hl.exec_cmd("darkman run")
+  hl.exec_cmd("uwsm app -- darkman run")
+  hl.exec_cmd("uwsm app -- keepassxc")
   hl.exec_cmd("gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font 12'")
   hl.exec_cmd("gsettings set org.gnome.desktop.interface font-name 'JetBrainsMono Nerd Font 12'")
   hl.exec_cmd("gsettings set org.gnome.desktop.interface monospace-font-name 'JetBrainsMono Nerd Font 12'")
